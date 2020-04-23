59218ffa0b046014556c843a12de4a1ad54ee53f
yenying <cindy19951211@gmail.com>
2020-04-23 21:14:30 +0800

    Resolve
    Resolve #1 implement findLabel function

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,16 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+			//假如在第一層就找到 直接return label
+	       const targetPermission = permissions.find(permission=>permission.code ===code)
+	       if(targetPermission)  return targetPermission.label
+           
+           //攤平一個多維陣列(相加)
+	       const childrenPermission =permissions.map(permission=>permission.children).reduce((x,y)=>x.concat(y))
+	       //console.log(childrenPermission)
+           
+           //假如陣列維空 則回傳null
+	       if(childrenPermission.length==0) return null
+	       else return findLabel(childrenPermission,code)  //不為空就遞迴
+	  
 }
