2f59e7a73d736bba5e3025876e4e3d71933e9e35
yenying <cindy19951211@gmail.com>
2020-04-23 22:31:05 +0800

    Update findLabel.spec.ts
    add unit test

--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -6,3 +6,22 @@ describe("findLabel spec", () => {
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
     });
 });
+
+describe("findLabel spec_return_string", () => {
+    it("should return string", () => {
+        expect(findLabel(mockPermissions, "code C")).toEqual(expect.any(String));
+    });
+});
+
+describe("findLabel spec_toEqual", () => {
+    it("should find Equal", () => {
+        expect(findLabel(mockPermissions, "code C-1")).toEqual("label C-1")
+    });
+});
+
+describe("findLabel spec_not_toBe", () => {
+    it("not find proper", () => {
+        expect(findLabel(mockPermissions, "code C-1-1")).not.toBe("label C-1-1")
+    });
+});
+
