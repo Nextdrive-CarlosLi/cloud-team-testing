fc905c02a051dd5dae321d2f2ec5439596470cb6
Jhu, Sin-Han <blueice0916@gmail.com>
2021-10-17 01:35:28 +0800

    feat(findLabel): completed findLabel feature and add test cases

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,12 @@
-import { Permission } from "./types/Permission";
+import { Permission } from './types/Permission'
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+	let result = null
+
+	permissions.forEach((premission) => {
+		if (premission.code === code) result = premission.label
+		else if (premission.children.length > 0 && result === null) result = findLabel(premission.children, code)
+	})
+
+	return result
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -1,8 +1,59 @@
-import { mockPermissions } from "../dummies/mockPermissions";
-import { findLabel } from "../src/findLabel";
+import { mockPermissions } from '../dummies/mockPermissions'
+import { findLabel } from '../src/findLabel'
 
-describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
-    });
-});
+describe('findLabel spec', () => {
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code A')).toBe('label A')
+	})
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code B')).toBe('label B')
+	})
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code B-1')).toBe('label B-1')
+	})
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code B-1-1')).toBe('label B-1-1')
+	})
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code B-2')).toBe('label B-2')
+	})
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code C')).toBe('label C')
+	})
+	it('should found', () => {
+		expect(findLabel(mockPermissions, 'code C-1')).toBe('label C-1')
+	})
+})
+
+describe('findLabel fail case', () => {
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'lable B')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'lable B-1')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code A-1')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code B-1-2')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code B-2-1')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code B-3')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code C-2')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code D')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'code E-1')).toBeNull()
+	})
+	it('should be null', () => {
+		expect(findLabel(mockPermissions, 'hello world')).toBeNull()
+	})
+})
