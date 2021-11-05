055475add3a868b71ae890b388b5a81dc509da32
Chun <yuchun9508@gmail.com>
2021-11-05 13:32:20 +0800

    Resolve #1 Implement the findLabel function

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,16 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+  const matchedPermission = permissions.find(
+    (permission) => permission.code === code
+  );
+  if (matchedPermission) return matchedPermission.label;
+
+  const childrenPermissions = permissions
+    .filter((permission) => permission.children.length)
+    .map((permission) => {
+      return permission.children;
+    });
+  const mergedPermissions = [].concat(...childrenPermissions);
+  return findLabel(mergedPermissions, code);
 }
