0310290a19a16a34567bb4f70a8cd61cce3a0c35
LinYenCheng <breaking0215@hotmail.com>
2021-12-24 13:36:04 +0800

    test(findLabel.spec.ts): 補 findLabel 測試案例

--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -3,6 +3,22 @@ import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
     it("should find proper", () => {
+        expect(findLabel(mockPermissions, "code A")).toBe("label A")
+    });
+
+    it("should find proper", () => {
+        expect(findLabel(mockPermissions, "code B")).toBe("label B")
+    });
+
+    it("should find proper", () => {
+        expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1")
+    });
+
+    it("should find proper", () => {
+        expect(findLabel(mockPermissions, "code C-1")).toBe("label C-1")
+    });
+
+    it("should find proper", () => {
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
     });
 });
