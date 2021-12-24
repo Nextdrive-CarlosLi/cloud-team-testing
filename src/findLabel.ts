97992b0ca5b7df270fb68aaae76d1ebec976862c
LinYenCheng <breaking0215@hotmail.com>
2021-12-24 13:35:24 +0800

    feat(findLabel.ts): 加入 findLabel 實作

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,27 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    const NOT_FOUND = 'not found';
+    const arrayFromCode = code.split(' ');
+    const arrCommend = arrayFromCode[1].split('-');
+
+    let currentNode: Permission;
+    let currentPermissions: Permission[];
+    let currentCode = arrayFromCode[0];
+    let strCurrentResult = NOT_FOUND;
+
+    arrCommend.forEach((commend, index) => {
+        if (index === 0) {
+            // 組第一次不用 - 也不用抓 children
+            currentPermissions = [...permissions];
+            currentCode = `${currentCode} ${commend}`;
+        } else if (currentNode.children && currentNode.children.length > 0) {
+            currentPermissions = [...currentNode.children];
+            currentCode = `${currentCode}-${commend}`;
+        }
+        currentNode = currentPermissions.find((node) => node.code === currentCode);
+        if (currentNode.label) strCurrentResult = currentNode.label;
+    });
+
+    return strCurrentResult;
 }
