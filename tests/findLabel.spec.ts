f4209e41116d774636b6ce699266083ea1645460
tsai4n <aiko3310@hotmail.com>
2021-12-31 13:55:12 +0800

    add: test

--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -1,8 +1,20 @@
 import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
-
+import {NOT_ALLOWED,CANT_FIND} from '../src/constant';
 describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+    it("should find A", () => {
+        expect(findLabel(mockPermissions, mockPermissions[0].code)).toBe(mockPermissions[0].label)
     });
+    it("should find B-1", () => {
+        expect(findLabel(mockPermissions, mockPermissions[1].children[0].code)).toBe(mockPermissions[1].children[0].label)
+    });
+    it("should find B-1-1", () => {
+        expect(findLabel(mockPermissions, mockPermissions[1].children[0].children[0].code)).toBe(mockPermissions[1].children[0].children[0].label)
+    });
+    it("should be not allowed", () => {
+        expect(findLabel(mockPermissions,"code")).toBe(NOT_ALLOWED)
+    })
+    it("should be can't find", () => {
+        expect(findLabel(mockPermissions,"coDe B-01")).toBe(CANT_FIND)
+    })
 });
