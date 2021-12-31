4e198db40c918b6cbb4ffbd2f197f1ab682360ae
tsai4n <aiko3310@hotmail.com>
2021-12-31 14:13:15 +0800

    feat: add findLabel function

---
+++ src/constant.ts
@@ -0,0 +1,2 @@
+export const NOT_ALLOWED:string = 'Not allowed'
+export const CANT_FIND:string = `can't find`
--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,29 @@
 import { Permission } from "./types/Permission";
-
+import { NOT_ALLOWED, CANT_FIND } from "./constant";
+const ALLOW_CODE_SPLIT_LENGTH: number = 2;
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+  const splitOriginCode: string[] = code.split(" ");
+  if (splitOriginCode.length !== ALLOW_CODE_SPLIT_LENGTH) {
+    return NOT_ALLOWED;
+  }
+  const originCode: string[] = splitOriginCode[1].split("-");
+  const findTreeNode = (
+    newPermissions: Permission[],
+    treeNodeArr: string[],
+    originNode: string
+  ):string => {
+    const findTreeLabel: Permission|undefined = newPermissions.find(
+      permission => permission.code === `${splitOriginCode[0]} ${originNode}`
+    );
+    if (findTreeLabel && treeNodeArr.length > 1) {
+      const newTreeNodeArr: string[] = treeNodeArr.filter((_, i) => i !== 0);
+      return findTreeNode(
+        findTreeLabel.children,
+        newTreeNodeArr,
+        `${originNode}-${newTreeNodeArr[0]}`
+      );
+    }
+    return findTreeLabel ? findTreeLabel.label : CANT_FIND;
+  };
+  return findTreeNode(permissions, originCode, originCode[0]);
 }
