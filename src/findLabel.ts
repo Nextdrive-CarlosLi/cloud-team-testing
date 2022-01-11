4690f8cbd39d306f1ac623a5f9e3acd1091f1a90
blank.chen <blank.chen@kiwigame.com>
2022-01-11 09:36:47 +0800

    finish test

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,26 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+  if (permissions.length === 0) {
+    return null;
+  }
+  
+  let result = null;
+  for (let i = 0; i < permissions.length; i++) {
+    const permission = permissions[i];
+    // if match code then return
+    if (permission.code === code) {
+      return permission.label;
+    }
+    // check is Array then recursive children
+    if (Array.isArray(permission.children)) {
+        result = findLabel(permission.children, code);
+    }
+    // if find result break for loop
+    if (!!result) {
+      break;
+    }
+  }
+
+  return result;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,29 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+  describe("should find proper", () => {
+    test.each([
+      ["code A", "label A"],
+      ["code B", "label B"],
+      ["code B-1", "label B-1"],
+      ["code B-1-1", "label B-1-1"],
+      ["code B-2", "label B-2"],
+      ["code C", "label C"],
+      ["code C-1", "label C-1"],
+    ])("given %p as arguments, returns %p", (code, label) => {
+      const result = findLabel(mockPermissions, code);
+      expect(result).toEqual(label);
     });
+  });
+
+  describe("should find null", () => {
+    test.each([
+      ["code D", null],
+      ["", null],
+      [null, null],
+    ])("given %p as arguments, returns %p", (code, label) => {
+      const result = findLabel(mockPermissions, code);
+      expect(result).toEqual(label);
+    });
+  });
 });
