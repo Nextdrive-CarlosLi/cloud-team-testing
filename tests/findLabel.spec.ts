c50e5740d5acea51db0d262afd7d555cc81d8d60
Ashu Cai <yfcai1227@gmail.com>
2022-03-15 00:34:49 +0800

    test: add test case

--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -4,12 +4,12 @@ import { findLabel } from "../src/findLabel";
 describe("findLabel spec", () => {
     it("should find proper", () => {
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
-        expect(findLabel(mockPermissions, "code A-1-1")).toBe("ERROR INPUT")
-        expect(findLabel(mockPermissions, "code A")).toBe("label A")
         expect(findLabel(mockPermissions, "code B-2-2-1")).toBe("ERROR INPUT")
-        expect(findLabel(mockPermissions, "code ")).toBe("ERROR INPUT")
+        expect(findLabel(mockPermissions, "code A")).toBe("label A")
         expect(findLabel(mockPermissions, "code C")).toBe("label C")
-
+        expect(findLabel(mockPermissions, "code D")).toBe("ERROR INPUT")
+        expect(findLabel(mockPermissions, "code ")).toBe("ERROR INPUT")
+        expect(findLabel(mockPermissions, "code A-1-1")).toBe("ERROR INPUT")
     });
 });
 
