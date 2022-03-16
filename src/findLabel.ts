9005f3e6c2e3656047df47537a89d01d6d234f65
yf24 <coopldh@gmail.com>
2022-03-16 09:14:18 +0800

    [Feature] impl. findLabel

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,45 @@
 import { Permission } from "./types/Permission";
 
+class ExtractedCode {
+    symbol: string;
+    position: number[];
+    constructor(symbol: string, position: number[]) {
+        this.symbol = symbol
+        this.position = position
+    }
+}
+
+function extractCode(code: string): ExtractedCode {
+    let targetStr = code.split(' ')[1];
+    let splitTargetStr = targetStr.split('-');
+    let symbol = splitTargetStr.shift();
+    let position: number[] = (splitTargetStr.length > 0)
+        ? splitTargetStr.map(x => Number(x))
+        : [];
+    return new ExtractedCode(symbol, position);
+}
+
+function validateInput(code: string): boolean {
+    let regex = new RegExp("^code [a-z]+$|(?<=code [a-z])(-[0-9])+$");
+    return (code) ? regex.test(code) : false;
+}
+
+
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    let _code = code.toLowerCase();
+    if (validateInput(_code)) {
+        let extractedCode = extractCode(_code);
+        var permission = permissions.find(x => extractCode(x.code.toLowerCase()).symbol == extractedCode.symbol);
+        extractedCode.position.every(function(index) {
+            if (index > permission.children.length) { 
+                permission = null
+                return false; 
+            } 
+            permission = permission.children[index - 1]
+            return true
+        });     
+        return (permission) ? permission.label : null
+    } else {
+        throw new Error("invalid code.");
+    }
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,37 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
+    it("should validate input code", () => {
+        let expected = new Error("invalid code.");
+        expect(() => { findLabel(mockPermissions, "label A"); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code A-"); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code 0-1"); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code A-1."); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "codeA-1-1"); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code A?1?2"); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, ""); }).toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "!*#&(!^%"); }).toThrowError(expected)
+
+        expect(() => { findLabel(mockPermissions, "code A"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code AA"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code A-1"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code A-1-1"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "Code A"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "CODE A"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "code a"); }).not.toThrowError(expected)
+        expect(() => { findLabel(mockPermissions, "CoDe A"); }).not.toThrowError(expected)
+    });
+
     it("should find proper", () => {
+        expect(findLabel(mockPermissions, "code A")).toBe("label A")
+        expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1")
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+    });
+
+    it("should NOT find proper", () => {
+        expect(findLabel(mockPermissions, "code AA")).toBe(null)
+        expect(findLabel(mockPermissions, "code B-3")).toBe(null)
+        expect(findLabel(mockPermissions, "code B-1-2")).toBe(null)
+        expect(findLabel(mockPermissions, "code D")).toBe(null)
     });
 });
