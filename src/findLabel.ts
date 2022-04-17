f8fa59ec11953c907103f544c2d75d5359e7ea17
Wesley Wang <Wesley.Wang@dlinkcorp.com>
2022-04-17 18:12:51 +0800

    Implement findLabel

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,16 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    let newPermissions: Permission[] = permissions;
+    let resultLabel: string = '';
+
+    for(let i: number = 0 ; i < newPermissions.length ; i ++){
+        if(newPermissions[i]['code'] == code){
+            resultLabel = newPermissions[i]['label'];
+        }else if(!!newPermissions[i]['children'] && newPermissions[i]['children'].length > 0){
+            newPermissions = [].concat(newPermissions, newPermissions[i]['children']);
+        }
+    }
+
+    return resultLabel;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,31 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+    test("should find proper to A", () => {
+        expect(findLabel(mockPermissions, "code A")).toBe("label A");
+    });
+
+    test("should find proper to B", () => {
+        expect(findLabel(mockPermissions, "code B")).toBe("label B");
+    });
+
+    test("should find proper to C", () => {
+        expect(findLabel(mockPermissions, "code C")).toBe("label C");
+    });
+
+    test("should find proper to B-1", () => {
+        expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1");
+    });
+
+    test("should find proper to B-2", () => {
+        expect(findLabel(mockPermissions, "code B-2")).toBe("label B-2");
+    });
+
+    test("should find proper to B-1-1", () => {
+        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1");
+    });
+
+    test("should find prppoer to empty string", () => {
+        expect(findLabel(mockPermissions, "code B-3")).toBe("");
     });
 });
