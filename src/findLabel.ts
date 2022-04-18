dd584464ffb63bf1a1ff0146143ba249c7062288
FateXRebirth <FateXRebirth@gmail.com>
2022-04-19 00:51:36 +0800

    Implement findLabel

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,9 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    for(const permission of permissions) {
+        if(permission.code === code) return permission.label;
+        if(permission.children.length !== 0) return findLabel(permission.children, code);
+    }
+    return 'not found';
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,15 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
+    it("should find nothing", () => {
+        expect(findLabel(mockPermissions, "code D")).toBe("not found");
+    });
+
     it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+        expect(findLabel(mockPermissions, "code A")).toBe("label A");
+    });
+
+    it("should find deeper", () => {
+        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1");
     });
 });
