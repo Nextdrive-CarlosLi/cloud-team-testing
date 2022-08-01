810664aac74bdcf3fa5bcf2dbe6264173f30b1ce
Rubylago <rubybibi6@gmail.com>
2022-08-01 19:27:06 +0800

    test done

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,19 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    const thisLevel = permissions.find(permission => {
+        return permission.code === code 
+    })
+    if(thisLevel) return thisLevel.label
+    // 以上可以找到所在的層
+    // 如果裡面的children.length > 0 表示還不是最後一層，再跑一次
+    let innerLevel = []
+    permissions.forEach(permission => {
+    const permissions = permission.children
+    if (permissions && permissions.length > 0){
+        innerLevel.push(findLabel(permissions, code))
+        }
+    })  
+    return innerLevel[0]
 }
+
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -3,6 +3,12 @@ import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
     it("should find proper", () => {
+        expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1")
+    });
+});
+
+describe("findLabel spec", () => {
+    it("should find proper", () => {
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
     });
 });
