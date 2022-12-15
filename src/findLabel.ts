2b2a3c715bcf90e45204dd87ebc58d9c9ce4e62d
Matt Tai <listennn08@outlook.com>
2022-12-15 21:50:40 +0800

    feat: implement findLabel function

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,15 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    let res: string;
+
+    for (let i = 0; i < permissions.length; i++) {
+        if (permissions[i].code === code) return permissions[i].label;
+        if (permissions[i].children && permissions[i].children.length > 0) {
+            const ret = findLabel(permissions[i].children, code);
+            if (ret) res = ret;
+        }
+    }
+
+    return res;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,23 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+    // arrange
+    const testCases = ["A", "B-1-1", "C-1"];
+    for (let i = 0; i < testCases.length; i++) {
+        // action
+        const ret = findLabel(mockPermissions, `code ${testCases[i]}`);
+        it(`should find code ${testCases[i]}`, () => {
+            // assert
+            expect(ret).toBe(`label ${testCases[i]}`);
+        });
+    }
+
+    it("should not find C-1-1", () => {
+        // arrange
+        const code = "code C-1-1";
+        // action
+        const ret = findLabel(mockPermissions, code);
+        // assert
+        expect(ret).toBe(undefined);
     });
 });
