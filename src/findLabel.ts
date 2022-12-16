defc1d1e2c84f403db0ab53c6ef914b382b37ad0
tzuyi <a0988426059@gmail.com>
2022-12-16 12:42:18 +0800

    Implement: findLabel function

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,18 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    const [type, levels] = code.split(" ");
+    if (!type || !levels) return "";
+    const levelList = levels.split("-");
+    let currentKey = type;
+
+    return levelList.reduce(({ permissions }, level, index) => {
+        currentKey += index === 0 ? ` ${level}` : `-${level}`;
+        const target = permissions.find(permission => permission.code === currentKey);
+
+        return {
+            permissions: target?.children ?? [],
+            label: target?.label ?? "",
+        };
+    }, { permissions, label: "" }).label;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -3,6 +3,18 @@ import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
     it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+        const testPermissions = [
+            { code: "code A", label: "label A" },
+            { code: "code B-1-1", label: "label B-1-1" },
+            { code: "code B-2", label: "label B-2" },
+            { code: "code C-1", label: "label C-1" },
+            { code: "code D", label: "" },
+            { code: "test E", label: "" },
+            { code: "", label: "" },
+        ];
+
+        testPermissions.forEach(({ code, label }) => {
+            expect(findLabel(mockPermissions, code)).toBe(label);
+        });
     });
 });
