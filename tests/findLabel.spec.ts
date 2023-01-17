84adb2a5513d6fe37db50bb2ec2c075a639dbf99
Merchuli <30795240+merchuli@users.noreply.github.com>
2023-01-17 11:29:37 +0800

    feat: Add test case to findLabel.spec.ts

--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -3,6 +3,10 @@ import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
     it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1");
+    });
+
+    it("should be undefined", () => {
+        expect(findLabel(mockPermissions, "code A-1")).toBeUndefined();
     });
 });
