c121899371320f0b68b4001df13d41c24e2cc499
Merchuli <30795240+merchuli@users.noreply.github.com>
2023-01-17 11:24:19 +0800

    feat: Implement findLabel

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,29 @@
 import { Permission } from "./types/Permission";
 
-export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+export function findLabel(permissions: Permission[], code: string): string {    
+  const level: Array<string> = code.replace('code ', '').split('-');
+  let current: Permission[] = permissions;
+  let i = 0;
+  let targetLabel = undefined;
+  
+  while (i < level.length) {
+    const targetIndex = current.findIndex((item) => {
+      return code.includes(item.code);
+    });
+
+    if (targetIndex === -1) {
+      break;
+    }
+
+    if (current[targetIndex].code === code) {
+      targetLabel = current[targetIndex].label;
+      break;
+    }
+
+    current = current[targetIndex].children;
+    i += 1;
+  }
+
+  
+  return targetLabel;
 }
