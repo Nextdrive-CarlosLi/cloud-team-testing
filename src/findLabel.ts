a892b0c24b1fed0cc2d304d7435340f6c941538a
goldsheep666 <wolf125tw100@gmail.com>
2023-07-09 21:16:36 +0800

    add function findLabel

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,14 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    for (let i = 0; i < permissions.length; i++) {
+        if (permissions[i].code === code) {
+          return permissions[i].label;
+        }
+    
+        if (permissions[i].children.length !== 0) {
+          return findLabel(permissions[i].children, code);
+        }
+      }
+      return undefined;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -3,6 +3,9 @@ import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
     it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+        expect(findLabel(mockPermissions, "code B")).toBe("label B");
+        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1");
+        expect(findLabel(mockPermissions, "code B-2-2")).toBe(undefined);
+        expect(findLabel([], "code A")).toBe(undefined);
     });
 });
