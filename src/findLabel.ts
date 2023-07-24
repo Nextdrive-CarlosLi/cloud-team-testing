3ece5ae1bb1ab4ceb70b5f970d37b462e96dbc6e
cching01099 <cching01099@gmail.com>
2023-07-24 17:31:06 +0800

    Implement findLabel function and test cases

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,15 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+  for (const permission of permissions) {
+    if (permission.code === code) {
+      return permission.label;
+    }
+    //check if the current permission has children or not
+    if (permission.children.length > 0) {
+      return findLabel(permission.children, code);
+    }
+  }
+  //after traverse, still cannot find the matching permission
+  return undefined;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,14 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
-    });
+  //correct matching
+  it("should find proper", () => {
+    expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1");
+    expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1");
+  });
+  //not matching or empty array
+  it("should return undefined for non-existing code or empty array", () => {
+    expect(findLabel(mockPermissions, "code A-1")).toBe(undefined);
+    expect(findLabel([], "code C")).toBe(undefined);
+  });
 });
