8db9a476645ee87736e0fd0328d26a197cd6a988
Jordan <jordantseng1024@gmail.com>
2023-09-27 00:34:18 +0800

    Add findLabel test cases

--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -1,8 +1,16 @@
-import { mockPermissions } from "../dummies/mockPermissions";
-import { findLabel } from "../src/findLabel";
+import { mockPermissions } from '../dummies/mockPermissions';
+import { findLabel } from '../src/findLabel';
 
-describe("findLabel spec", () => {
-    it("should find proper", () => {
-        expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
-    });
+describe('findLabel spec', () => {
+  it('should find improperly due to empty permissions', () => {
+    expect(findLabel([], 'code B-1-1')).toBeNull();
+  });
+
+  it('should find properly', () => {
+    expect(findLabel(mockPermissions, 'code B-1-1')).toBe('label B-1-1');
+  });
+
+  it('should find improperly', () => {
+    expect(findLabel(mockPermissions, 'code Z')).toBeNull();
+  });
 });
