1b71c990a1b2cabb41e1b3bd8f5914414f44f272
Jordan <jordantseng1024@gmail.com>
2023-09-27 00:00:23 +0800

    submit testing solution

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,66 @@
-import { Permission } from "./types/Permission";
+import { Permission } from './types/Permission';
 
+// dfs
+// n = maximum number of permissions
+// Time: O(n)
+// Space: O(n)
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+  if (permissions.length === 0) {
+    return '';
+  }
+
+  for (const permission of permissions) {
+    const label = dfs(permission, code);
+    if (label) {
+      return label;
+    }
+  }
+
+  return '';
 }
+
+function dfs(permission: Permission, code: string) {
+  if (permission.code === code) {
+    return permission.label;
+  }
+
+  for (const child of permission.children) {
+    const label = dfs(child, code);
+    if (label) {
+      return label;
+    }
+  }
+
+  return null;
+}
+
+// bfs
+// n = maximum number of permissions
+// Time: O(n)
+// Technically, time complexity is O(n^2), because time complexity of shift method in js is O(n)
+// Space: O(n)
+// export function findLabel(permissions: Permission[], code: string): string {
+//   if (permissions.length === 0) {
+//     return '';
+//   }
+
+//   const queue = [];
+
+//   for (const permission of permissions) {
+//     queue.push(permission);
+//   }
+
+//   while (queue.length > 0) {
+//     const current = queue.shift();
+
+//     if (current.code === code) {
+//       return current.label;
+//     }
+
+//     for (const child of current.children) {
+//       queue.push(child);
+//     }
+//   }
+
+//   return '';
+// }
