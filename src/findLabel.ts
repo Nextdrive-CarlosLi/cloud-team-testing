017a7e3fded82492aecf2740e31203802e32a515
jimmyww7710 <36302780+jimmyww7710@users.noreply.github.com>
2023-10-01 10:14:42 +0800

    complete functionality of findLabel and test file

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,31 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+    let resultLabel = "";
+    const findMathedLabel = function (obj: Permission, code: string) {
+        if (!obj) {
+            return;
+        }
+        else if (obj?.code === code) {
+            return obj?.label;
+        }
+        else {
+            for (let i = 0; i < obj.children.length; i++) {
+                const matchedLabel = findMathedLabel(obj.children[i], code);
+                if (matchedLabel) {
+                    return matchedLabel;
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i < permissions.length; i++) {
+        const matchedLabel = findMathedLabel(permissions[i], code)
+        if (matchedLabel) {
+            resultLabel = matchedLabel;
+            break
+        }
+    }
+
+    return resultLabel;
 }
--- tests/findLabel.spec.ts
+++ tests/findLabel.spec.ts
@@ -2,7 +2,25 @@ import { mockPermissions } from "../dummies/mockPermissions";
 import { findLabel } from "../src/findLabel";
 
 describe("findLabel spec", () => {
-    it("should find proper", () => {
+    it("should find proper label A", () => {
+        expect(findLabel(mockPermissions, "code A")).toBe("label A")
+    });
+    it("should find proper label B", () => {
+        expect(findLabel(mockPermissions, "code B")).toBe("label B")
+    });
+    it("should find proper label B-1", () => {
+        expect(findLabel(mockPermissions, "code B-1")).toBe("label B-1")
+    });
+    it("should find proper label B-1-1", () => {
         expect(findLabel(mockPermissions, "code B-1-1")).toBe("label B-1-1")
+    });
+    it("should find proper label B-2", () => {
+        expect(findLabel(mockPermissions, "code B-2")).toBe("label B-2")
+    });
+    it("should find proper label C", () => {
+        expect(findLabel(mockPermissions, "code C")).toBe("label C")
+    });
+    it("should find proper label C-1", () => {
+        expect(findLabel(mockPermissions, "code C")).toBe("label C-1")
     });
 });
