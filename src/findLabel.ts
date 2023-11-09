0eba6cebd93387a18c6cab06f19275038274a421
Arvin <Arvinchen1995@gmail.com>
2023-11-10 00:34:16 +0800

    implement findLabel

--- src/findLabel.ts
+++ src/findLabel.ts
@@ -1,5 +1,20 @@
 import { Permission } from "./types/Permission";
 
 export function findLabel(permissions: Permission[], code: string): string {
-    return "label B-1-1"; // TODO
+	let ret = "Not Found";
+	const dfs = (permissions: Permission[], code: string) => {
+		for (const permission of permissions)
+		{
+			if (code === permission.code)
+			{
+				ret = permission.label;
+				return true;
+			}
+			if (permission.children.length === 0) continue;
+			if (dfs(permission.children, code)) return true;
+		}
+		return false;
+	}
+	dfs(permissions, code);
+	return ret;
 }
